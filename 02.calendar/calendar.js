2282f15ecbf10070e40d90ef122a1b8c26cef3ea
su-su-su-su <suzukikoji0503@gmail.com>
2023-11-21 19:01:56 +0900

    変数の定数化と命名変更、カレンダー出力の改善

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -5,18 +5,19 @@ const argv = minimist(process.argv.slice(2), {
     y: "year",
   },
 });
-let now = new Date();
-let year = argv.year || now.getFullYear();
-let month = argv.month || now.getMonth() + 1;
-let day_first = new Date(year, month - 1, 1);
-let day_end = new Date(year, month, 0);
-console.log(year + "年" + month + "月");
-console.log(" 日 月 火 水 木 金 土");
-process.stdout.write(" ".repeat(3 * day_first.getDay()));
-while (day_first <= day_end) {
-  process.stdout.write(day_first.getDate().toString().padStart(3, " "));
-  if (day_first.getDay() === 6) {
+const now = new Date();
+const year = argv.year || now.getFullYear();
+const month = argv.month || now.getMonth() + 1;
+const currentDate = new Date(year, month - 1, 1);
+const dayEnd = new Date(year, month, 0);
+console.log(`${month.toString().padStart(8, " ")}月 ${year}`);
+console.log("日 月 火 水 木 金 土");
+process.stdout.write(" ".repeat(3 * currentDate.getDay()));
+while (currentDate <= dayEnd) {
+  process.stdout.write(currentDate.getDate().toString().padStart(3, " "));
+  if (currentDate.getDay() === 6) {
     process.stdout.write("\n");
   }
-  day_first.setDate(day_first.getDate() + 1);
+  currentDate.setDate(currentDate.getDate() + 1);
 }
+process.stdout.write("\n");
