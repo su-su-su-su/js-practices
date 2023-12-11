aae7d24debd7e7f81cd4681d1444e36770df5635
su-su-su-su <suzukikoji0503@gmail.com>
2023-12-12 00:11:09 +0900

    不要なエラーハンドリングを削除

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,36 +1,21 @@
 import sqlite3 from "sqlite3";
 
-const db = new sqlite3.Database(":memory:", (err) => {
-  if (err) {
-    console.log(err.message);
-  }
+const db = new sqlite3.Database(":memory:", () => {
   console.log("データベースを作成");
 });
 
 db.run(
   "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  (err) => {
-    if (err) {
-      console.log(err.message);
-    }
+  () => {
     console.log("booksテーブルを作成");
 
-    db.run("INSERT INTO books(title) VALUES('JavaScriptの本')", function (err) {
-      if (err) {
-        console.log(err.message);
-      }
+    db.run("INSERT INTO books(title) VALUES('JavaScriptの本')", function () {
       console.log(`作成したレコードは${this.lastID}です`);
 
       db.get("SELECT * FROM books", (err, row) => {
-        if (err) {
-          console.log(err.message);
-        }
         console.log(`idは${row.id}です`);
 
-        db.run("DROP TABLE books", (err) => {
-          if (err) {
-            console.log(err.message);
-          }
+        db.run("DROP TABLE books", () => {
           console.log("booksテーブルを削除しました");
           db.close();
         });
