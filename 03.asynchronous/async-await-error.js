c07059a94aeb541d8f3cfb45d52cfd92109cdc0f
su-su-su-su <suzukikoji0503@gmail.com>
2023-12-12 10:29:37 +0900

    try...catchの範囲、例外の処理を修正

--- 03.asynchronous/async-await-error.js
+++ 03.asynchronous/async-await-error.js
@@ -2,32 +2,36 @@ import sqlite3 from "sqlite3";
 import { run, get } from "./database-functions.js";
 
 async function main() {
+  const db = new sqlite3.Database(":memory:");
+  console.log("データベースを作成");
+
+  await run(
+    db,
+    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+  );
+  console.log("booksテーブルを作成");
   try {
-    const db = new sqlite3.Database(":memory:");
-    console.log("データベースを作成");
-
-    await run(
-      db,
-      "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
-    );
-    console.log("booksテーブルを作成");
-
-    const { err: runErr } = await run(
-      db,
-      "INSERT INTO book(title) VALUES('JavaScriptの本')"
-    );
-    if (runErr) console.error("エラー発生:", runErr.message);
-
-    const { err: getErr } = await get(db, "SELECT * FROM books WHERE notitle");
-    if (getErr) console.error("エラー発生:", getErr.message);
-
-    await run(db, "DROP TABLE books");
-    console.log("booksテーブルを削除しました");
-
-    db.close();
+    await run(db, "INSERT INTO book(title) VALUES('JavaScriptの本')");
   } catch (err) {
-    console.error("データベース操作中にエラーが発生しました:", err.message);
+    if (err instanceof Error) {
+      console.error("エラー発生:", err.message);
+    } else {
+      throw err;
+    }
   }
+  try {
+    await get(db, "SELECT * FROM books WHERE notitle");
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error("エラー発生:", err.message);
+    } else {
+      throw err;
+    }
+  }
+  await run(db, "DROP TABLE books");
+  console.log("booksテーブルを削除しました");
+
+  db.close();
 }
 
 main();
